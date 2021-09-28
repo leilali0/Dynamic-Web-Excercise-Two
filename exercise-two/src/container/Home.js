baf13cdb042bafd1007401a716d28da87881e30a
leilali0 <46583372+leilali0@users.noreply.github.com>
2021-09-28 09:21:04 -0400

    create router

--- exercise-two/src/container/Home.js
+++ exercise-two/src/container/Home.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useMemo } from "react";
 
 
 function Home(){
+
+const { weaterType } = useMemo(() => {
+  return {
+    couldyness: 100,
+    currentTemp: `76`,
+    highTemp: `80`,
+    humidity: 100,
+    lowTemp: `80`,
+    weatherType: "Cloudy",
+    windSpeed: `10mph`,
+  }
+})
+
   return (
     <main ClassName="App">
       <header>Weather App</header>
